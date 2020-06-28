946767cd0e42c12a9b40489980f9e173eafaa4f7
Amana Ammishaddai <amana.ammi@gmail.com>
2020-06-28 03:45:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+# %w(some words) is simply a shortcut for instantiating an array with items in it!
+
+class Anagram 
+  
+  attr_accessor :word
+  
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match
+    #iteration
+    #possible_anagrams = []
+    #returns all matches ==  []
+  #||
+    #no_matches = just an empty array 
+  end
+  
+end 
